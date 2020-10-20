7404cd921c7f7d950a510d79d3fbf83a69f0ff5f
gustavoghp87 <ghp.2120@gmail.com>
2020-10-19 22:42:23 -0300

    correc counter

--- src/controllers/counter.ts
+++ src/controllers/counter.ts
@@ -6,28 +6,29 @@ export const counter = async (req:any, res:any) => {
 
     if (req.body.password!==process.env.COUNTER_PW) return res.status(400).json({msg:'Wrong password'})
   
+    let json = []
     let axios:any, data:any
-  
+
     axios = await Axios(`https://api.countapi.xyz/get/maslabook/${counterPsw}`)
     data = await axios.data
-    let json:any = {counter:data.value}
-  
+    json.push( {maslabook: data.value} )
+
     axios = await Axios(`https://api.countapi.xyz/get/maslastory/${counterPsw}`)
     data = await axios.data
-    json.push( {counter:data.value})
-  
+    json.push( {maslastory: data.value} )
+
     axios = await Axios(`https://api.countapi.xyz/get/maslaboard/${counterPsw}`)
     data = await axios.data
-    json.push( {counter:data.value})
-  
+    json.push( {maslaboard: data.value} )
+
     axios = await Axios(`https://api.countapi.xyz/get/maslazoom/${counterPsw}`)
     data = await axios.data
-    json.push( {counter:data.value})
-  
+    json.push( {maslazoom: data.value} )
+
     axios = await Axios(`https://api.countapi.xyz/get/maslanomics/${counterPsw}`)
     data = await axios.data
-    json.push( {counter:data.value})
-  
+    json.push( {maslanomics: data.value} )
+
     console.log(json)
     res.status(200).json(json)
 }
